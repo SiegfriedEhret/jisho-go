a5683a6fff34043f5793932b57a16e878d51b25c
Siegfried Ehret <siegfried@ehret.me>
2016-02-21 21:06:30 +0100

    :gift: update main thing

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("jisho")
+	fmt.Println("jisho", os.Args)
+
+	db, err := sql.Open("sqlite3", "./data/kanjidb.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
+
+/*
+func search(db, kanji) {
+
+}
+*/
+
+/*
+function search(kanji, cb) {
+  doSearch(`SELECT *
+    FROM elements AS E
+    JOIN kanjidict AS K ON E.kanji = K.kanji
+    WHERE E.kanji = '${kanji}'`, cb);
+}
+
+function searchSentence(kanji, cb) {
+  doSearch(`SELECT *
+    FROM sentences
+    WHERE kanji LIKE '%${kanji}%';`, cb);
+}
+
+function doSearch(query, cb) {
+  debug('Query:', query);
+
+  db.all(query, cb);
+}
+*/

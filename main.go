a8498e1b84af38425a89c22ff1f24920e4ef61d5
Siegfried Ehret <siegfried@ehret.me>
2016-02-23 06:59:29 +0100

    :gift: search only one kanji ^^

--- main.go
+++ main.go
@@ -9,22 +9,68 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// KanjiData represents a kanji
+type KanjiData struct {
+	Kanji    string
+	Meaning  string
+	Onyomi   string
+	Kunyomi  string
+	Elements string
+	PartOf   string
+	Jlpt     string
+	Strokes  int
+}
+
 func main() {
 	fmt.Println("jisho", os.Args)
 
 	db, err := sql.Open("sqlite3", "./data/kanjidb.sqlite")
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	KanjiData := search(db, "冥")
+
+	fmt.Println(KanjiData)
+
 	defer db.Close()
 }
 
-/*
-func search(db, kanji) {
+func search(db *sql.DB, kanji string) KanjiData {
+	query := `SELECT
+			E.kanji,
+			E.strokes,
+			K.meaning,
+			onyomi,
+			kunyomi,
+			elements,
+			part_of,
+			jlpt
+		FROM elements AS E
+		JOIN kanjidict AS K ON E.kanji = K.kanji
+		WHERE E.kanji = '` + kanji + "'"
 
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var kanjiData KanjiData
+
+	for rows.Next() {
+		var kanji, meaning, onyomi, kunyomi, elements, partOf, jlpt string
+		var strokes int
+
+		rows.Scan(&kanji, &strokes, &meaning, &onyomi, &kunyomi, &elements, &partOf, &jlpt)
+
+		kanjiData = KanjiData{kanji, meaning, onyomi, kunyomi, elements, partOf, jlpt, strokes}
+
+	}
+
+	return kanjiData
 }
-*/
 
 /*
 function search(kanji, cb) {

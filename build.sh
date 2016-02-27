23339f74dc0150583fb3eb942c5f74338910915d
Siegfried Ehret <siegfried@ehret.me>
2016-02-27 10:53:44 +0100

    :wrench: fix compilation thing

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-rm -f jisho jisho.go
-go build -o jisho main.go
+
+#rm -f jisho jisho.go
+#go build -o jisho main.go
+
+# gox -output="build/{{.OS}}/{{.Arch}}/{{.Dir}}" -osarch="linux/amd64 darwin/amd64 windows/amd64" -verbose
+CGO_ENABLED=1 gox -output="build/" -osarch="linux/amd64" -verbose

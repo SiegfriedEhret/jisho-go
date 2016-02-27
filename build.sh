b5e75ef69c2b78fbf8612f61e29cf25c43efd0a6
Siegfried Ehret <siegfried@ehret.me>
2016-02-27 11:00:10 +0100

    :wrench: update build script

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 #rm -f jisho jisho.go
 #go build -o jisho main.go
 
-# gox -output="build/{{.OS}}/{{.Arch}}/{{.Dir}}" -osarch="linux/amd64 darwin/amd64 windows/amd64" -verbose
-CGO_ENABLED=1 gox -output="build/" -osarch="linux/amd64" -verbose
+CGO_ENABLED=1 gox -output="build/{{.OS}}/{{.Arch}}/{{.Dir}}" -osarch="linux/amd64 darwin/amd64 windows/amd64"
